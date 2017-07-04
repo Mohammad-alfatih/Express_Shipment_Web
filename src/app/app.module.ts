75797cd4634bd9082bb9ff315cf2e3ff6d66cdbf
Hamada <Hamada@Hamada-MBP.local>
2017-07-04 17:04:44 -0400

    basic homepage with jumbotron & quote form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { JumbotronComponent } from './components/jumbotron/jumbotron.component';
+import { BodyComponent } from './components/body/body.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { QuoteFormComponent } from './components/forms/quote-form/quote-form.component';
+import { LoginFormComponent } from './components/forms/login-form/login-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    JumbotronComponent,
+    BodyComponent,
+    FooterComponent,
+    QuoteFormComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule
